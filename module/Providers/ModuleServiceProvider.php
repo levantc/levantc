e8e448c4e729a10eb22c641b79504b3c29c56b45
Muath Abu Ouda <muathabuouda@hotmail.com>
2025-12-29 17:53:21 +0200

    init(module): setup module structure

---
+++ module/Providers/ModuleServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
