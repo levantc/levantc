516a31aea1efb9ddced090fa3542278c6062bd2d
Muath Abu Ouda <muathabuouda@hotmail.com>
2025-12-29 20:33:33 +0200

    feat(controllers): add base controller

---
+++ module/Factories/ResponderFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sadeem\Core\Module\Factories;
+
+use Sadeem\Core\Module\Enums\Responders\ResponderType;
+use Sadeem\Core\Module\Http\Responders\InertiaResponder;
+use Sadeem\Core\Module\Http\Responders\JsonResponder;
+use Sadeem\Core\Module\Http\Responders\RedirectBackResponder;
+use Sadeem\Core\Module\Http\Responders\Responder;
+
+/**
+ * Class ResponderFactory
+ *
+ * Factory responsible for creating Responder instances based on a given type.
+ *
+ * This approach allows explicit control over which responder to use, instead of
+ * relying on Request headers or content negotiation.
+ */
+class ResponderFactory
+{
+    /**
+     * Create a Responder instance based on the provided ResponderType.
+     *
+     * @param ResponderType $type
+     *   The type of responder to create (JSON, INERTIA, REDIRECT).
+     *
+     * @return Responder
+     *   An instance of the requested Responder.
+     */
+    public function make(ResponderType $type): Responder
+    {
+        return match ($type) {
+            ResponderType::JSON => new JsonResponder(),
+            ResponderType::INERTIA => new InertiaResponder(),
+            ResponderType::REDIRECT => new RedirectBackResponder(),
+        };
+    }
+}
--- module/Providers/ModuleServiceProvider.php
+++ module/Providers/ModuleServiceProvider.php
@@ -2,6 +2,7 @@
 namespace Sadeem\Core\Module\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Sadeem\Core\Module\Factories\RepositoryFactory;
 use Sadeem\Core\Module\Factories\UseCaseFactory;
 
 class ModuleServiceProvider extends ServiceProvider
@@ -14,6 +15,11 @@ class ModuleServiceProvider extends ServiceProvider
         // Register UseCaseFactory as a singleton for consistent use across the app
         $this->app->singleton(UsecaseFactory::class, function ($app) {
             return new UsecaseFactory($app);
+        });
+
+        // Register RepositoryFactory as a singleton for consistent use across the app
+        $this->app->singleton(RepositoryFactory::class, function ($app) {
+            return new RepositoryFactory($app);
         });
     }
 
