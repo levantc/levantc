be98875218edc446467c3415985d24709100443e
Muath Abu Ouda <muathabuouda@hotmail.com>
2026-02-25 15:43:14 +0200

    feat(provider): update

--- module/Providers/ModuleServiceProvider.php
+++ module/Providers/ModuleServiceProvider.php
@@ -28,7 +28,6 @@ class ModuleServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Load and register this module's routes'
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/module.php');
+        //
     }
 }
