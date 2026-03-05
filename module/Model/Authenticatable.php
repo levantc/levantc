4354b762cd5147e17a2de7203bae95875432f179
Muath Abu Ouda <muathabuouda@hotmail.com>
2026-03-05 15:32:25 +0200

    feat(auth): add Authenticatable abstraction as an alternative to Laravel auth

---
+++ module/Model/Authenticatable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sadeem\Core\Module\Model;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+abstract class Authenticatable extends User
+{
+    use Notifiable, TwoFactorAuthenticatable;
+
+    //
+}
