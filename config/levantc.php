e46a51cafe2c109d07c0a7556c72d4c70decfa95
Muath Abu Ouda <muathabuouda@hotmail.com>
2026-05-21 23:35:02 +0300

    feat(config): add LevantC configuration file for locale model

---
+++ config/levantc.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Locale Eloquent Model
+    |--------------------------------------------------------------------------
+    |
+    | Fully qualified class name of the Locale model used by LocaleHelper.
+    | Required only when using locale resolution helpers in a host application
+    | that provides internationalization data (for example, a dedicated i18n module).
+    |
+    */
+    'locale_model' => env('LEVANTC_LOCALE_MODEL'),
+
+];
